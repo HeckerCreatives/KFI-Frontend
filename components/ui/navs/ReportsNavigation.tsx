eb38f55e70d4bc1a9452797aa4d9742f082e54b1
ParadiseTeam69 <paradise.metro69@gmail.com>
2025-05-19 04:59:56 +0800

    deleted

--- components/ui/navs/ReportsNavigation.tsx
+++ components/ui/navs/ReportsNavigation.tsx
@@ -8,28 +8,13 @@ import classNames from 'classnames';
 const ReportsNavigation = () => {
   const pathname = usePathname();
 
-  const fileLinks: NavLink[] = [
-    { path: '/dashboard/soa-report', label: 'Statement of Acct.' },
-    { path: '/dashboard/cp-report', label: 'Client Profile' },
-    { path: '/dashboard/center-report', label: 'Center' },
-    { path: '/dashboard/wc-report', label: 'Weekly Collection' },
-    { path: '/dashboard/jv-report', label: 'Journal Voucher' },
-    { path: '/dashboard/ev-report', label: 'Expense Voucher' },
-    { path: '/dashboard/or-report', label: 'Official Receipt' },
-    { path: '/dashboard/lr-report', label: 'Loan Release' },
-    { path: '/dashboard/df-report', label: 'Damayan Fund' },
-    { path: '/dashboard/el-report', label: 'Emergency Loan' },
-    { path: '/dashboard/lrvor-report', label: 'Loan Release cs O.R.' },
-    { path: '/dashboard/pbd-report', label: 'Porject by Due date' },
-  ];
-
   return (
     <IonAccordion value="reports">
       <IonItem
         slot="header"
         className={classNames(
           '!text-[1rem] space-x-2 text-slate-500 [--padding-start:0.25rem] [--padding-end:0] hover:[--color:#FA6C2F]',
-          fileLinks.find((link: NavLink) => pathname === link.path) && '!text-[#fa6c2f]',
+          // fileLinks.find((link: NavLink) => pathname === link.path) && '!text-[#fa6c2f]',
         )}
       >
         <IonIcon size="small" icon={cellular} className="!text-inherit" />
@@ -37,7 +22,7 @@ const ReportsNavigation = () => {
       </IonItem>
       <div className="ion-padding" slot="content">
         <IonList className="p-0">
-          {fileLinks.map(link => (
+          {/* {fileLinks.map(link => (
             <IonMenuToggle key={link.path} autoHide={false}>
               <IonItem
                 routerLink={link.path}
@@ -49,7 +34,7 @@ const ReportsNavigation = () => {
                 <IonLabel>{link.label}</IonLabel>
               </IonItem>
             </IonMenuToggle>
-          ))}
+          ))} */}
         </IonList>
       </div>
     </IonAccordion>
