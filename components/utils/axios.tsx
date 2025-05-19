8aa78e1e72f3f65be1307002eeab8eb3d6fedaef
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2025-05-19 08:12:48 +0800

    Update axios.tsx

--- components/utils/axios.tsx
+++ components/utils/axios.tsx
@@ -1,7 +1,8 @@
 import axios, { AxiosError, AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 const kfiAxios: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+ // baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://kfi-api.onrender.com/api/v1',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
