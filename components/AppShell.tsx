c2ecefac4ed76bd8c8fd61b9c7bc6359d9aed0de
ParadiseTeam69 <113586211+charl191992@users.noreply.github.com>
2025-07-31 07:59:32 +0800

    rredirections

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -4,20 +4,27 @@ import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Login from './pages/auth/login/Login';
 import Tabs from './pages/Tabs';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
 setupIonicReact({});
 
 const AppShell = () => {
   const isLoggedIn = localStorage.getItem('auth') ? true : false;
+  const pathname = usePathname();
 
-  console.log(isLoggedIn);
+  useEffect(() => {
+    if (pathname !== '/' && !isLoggedIn) {
+      (window as any).location.href = '/';
+    }
+  }, [isLoggedIn]);
 
   return (
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet id="main">
-          <Route path="/" render={() => (isLoggedIn ? <Redirect to="/dashboard" /> : <Login />)} exact={true} />
-          <Route path="/dashboard" render={() => (isLoggedIn ? <Tabs /> : <Redirect to="/" />)} exact={true} />
+          <Route path="/" render={() => (isLoggedIn ? <Redirect to="/dashboard" /> : <Login />)} exact={isLoggedIn ? false : true} />
+          <Route path="/dashboard" render={() => (isLoggedIn ? <Tabs /> : <Redirect to="/" />)} exact={isLoggedIn ? false : true} />
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
