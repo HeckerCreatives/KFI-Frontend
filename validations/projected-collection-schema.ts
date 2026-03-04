3577286b48abfcdea9c903aa817698add9cca442
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2026-03-04 15:37:41 +0800

    chnages: pc schema validation

--- validations/projected-collection-schema.ts
+++ validations/projected-collection-schema.ts
@@ -1,14 +1,23 @@
 import z from 'zod';
 
 export const projectcollectiondocument = z.object({
-    startDate: z.string().nonempty('Start date is required'),
-    dueDateFrom: z.string().nonempty('Due date from is required'),
-    dueDateTo: z.string().nonempty('Due date from is required'),
-    center: z.string().min(1, 'Center Code is required').max(255, 'Center Code must only consist of 255 characters'),
-    centerLabel: z.string().min(1, 'Center Code is required').max(255, 'Center Code must only consist of 255 characters'),
-    accountOfficer: z.string().nonempty('Account officer is required'),
-    groupBy: z.string().nonempty('Group is required'),
-    type: z.string().nonempty('Type is required'),
+    // startDate: z.string().nonempty('Start date is required'),
+    // dueDateFrom: z.string().nonempty('Due date from is required'),
+    // dueDateTo: z.string().nonempty('Due date from is required'),
+    // center: z.string().min(1, 'Center Code is required').max(255, 'Center Code must only consist of 255 characters'),
+    // centerLabel: z.string().min(1, 'Center Code is required').max(255, 'Center Code must only consist of 255 characters'),
+    // accountOfficer: z.string().nonempty('Account officer is required'),
+    // groupBy: z.string().nonempty('Group is required'),
+    // type: z.string().nonempty('Type is required'),
+
+    startDate: z.string().optional(),
+    dueDateFrom: z.string().optional(),
+    dueDateTo: z.string().optional(),
+    center: z.string().optional(),
+    centerLabel: z.string().optional(),
+    accountOfficer: z.string().optional(),
+    groupBy: z.string().optional(),
+    type: z.string().optional(),
 
 
   
