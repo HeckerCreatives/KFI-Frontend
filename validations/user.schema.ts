3aae1ad29fc446b44b2060583dbedb33fb0bbe5a
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2026-03-16 17:15:55 +0800

    changes: create user password validation

--- validations/user.schema.ts
+++ validations/user.schema.ts
@@ -5,6 +5,9 @@ export const userSchema = z.object({
   username: z.string().min(1, 'Username is required'),
   password: z.string().min(1, 'Password is required'),
   confirm_password: z.string().min(1, 'Confirm Password is required'),
-});
+}).refine((data) => data.password === data.confirm_password, {
+    message: "Passwords do not match",
+    path: ["confirm_password"],
+  });
 
 export type UserFormData = z.infer<typeof userSchema>;
